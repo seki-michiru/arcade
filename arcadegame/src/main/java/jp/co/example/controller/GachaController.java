e130a295e0ca3e16aa6a24abd50e114f08d96ce0
nagahama <20nagahama_takatoshi@axiz.co.jp>
2020-06-18 17:58:17 +0900

    asd

--- arcadegame/src/main/java/jp/co/example/controller/GachaController.java
+++ arcadegame/src/main/java/jp/co/example/controller/GachaController.java
@@ -1,27 +1,47 @@
 package jp.co.example.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.example.entity.Items;
+import jp.co.example.service.GachaService;
 
 //mport jp.co.example.service.GachaService;
 
 @Controller
 public class GachaController {
-
-	//private GachaService gachaService;
+	@Autowired
+	private GachaService gachaService;
 
 	@RequestMapping("/gacha")
 	public String gacha(Model model) {
 
 		return "gacha";
 	}
+
 	@RequestMapping("/gachaPlay")
-	public String gachaPlay(Model model) {
+	public String gachaPlay(Model model, HttpSession session) {
 
+		int randomNumber = gachaService.Random();
+		List<Items> list = gachaService.gachaItem(randomNumber);
+		session.setAttribute("getItem", list);
 
+		//List<UserInfo> user = (List<UserInfo>) session.getAttribute("list");
+
+		gachaService.itemCollect(2, randomNumber);
+		gachaService.coinWast(2);
+		//
+		//		gachaService.itemCollect(user.get(0).getUserId(),randomNumber);
+		//		gachaService.coinWast(user.get(0).getUserId());
 		return "gachaPlay";
 	}
+
 	@RequestMapping("/gachaResult")
 	public String gachaResult(Model model) {
 
