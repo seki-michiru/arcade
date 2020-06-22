f9caa98336c928c2d4f82ea1f274d03191239eec
seiyatoma <stwit.18into1873@ezweb.co.jp>
2020-06-22 10:37:48 +0900

    buyform sellform追加

---
+++ arcadegame/src/main/java/jp/co/example/controller/form/SellForm.java
@@ -0,0 +1,26 @@
+package jp.co.example.controller.form;
+
+public class SellForm {
+
+	private Integer[] itemsId;
+	private Integer[] number;
+
+
+	public Integer[] getItemsId() {
+		return itemsId;
+	}
+
+	public void setItemsId(Integer[] itemsId) {
+		this.itemsId = itemsId;
+	}
+
+	public Integer[] getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer[] number) {
+		this.number = number;
+	}
+
+}
+
